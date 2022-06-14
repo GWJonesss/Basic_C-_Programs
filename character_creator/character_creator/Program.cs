d2ab0e81231bc726532fe539a4a2bfe61c3f4144
Gabriel Jones <gabrieljones900@yahoo.com>
2022-06-13 17:14:57 -0700

    ARRAYS SUBMISSION ASSIGNMENT
    C# AND UNITY COURSE Step: 182
    
    Create a console app the does/contains the following:
    
    Create a one-dimensional Array of strings. Ask the user to select an index of the Array and then display the string at that index on the screen.
    
    Create a one-dimensional Array of integers. Ask the user to select an index of the Array and then display the integer at that index on the screen.
    
    Add in a message that displays when the user selects an index that doesn’t exist.
    
    Create a list of strings. Ask the user to select an index of the list and then display the content at that index on the screen.

---
+++ character_creator/character_creator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    internal class Program
+    {
+    static void Main()
+    {
+
+
+        Console.WriteLine("Welcome to Character Creater. Please follow the instructions below.");
+
+        System.Threading.Thread.Sleep(1000);
+
+        Console.WriteLine("Pick your class:" + "\n1: Barbarian" + "\n2: Bard" + "\n3: Cleric" + 
+            "\n4: Druid"+ "\n5: Fighter" + "\n6: Monk" + "\n7: Paladin" + "\n8: Ranger");
+        Console.WriteLine("*ENTER NUMBER BETWEEN 1-8 TO INDICATE CHOICE*");
+        int cChoice = Convert.ToInt32(Console.ReadLine());
+
+
+        while (cChoice > 8 || cChoice < 1)
+        {
+            Console.WriteLine("OPS THAT WASN'T AN OPTION... *ENTER NUMBER BETWEEN 1-8 TO INDICATE CHOICE*");
+            cChoice = Convert.ToInt32(Console.ReadLine());
+
+        }
+
+        string[] classType = { "Barbarian", "Bard", "Cleric", "Druid", "Fighter", "Monk", "Paladin", "Ranger" };
+
+        int X = cChoice - 1;
+
+
+        Console.WriteLine("Choose your Strength Level (your choice will inversely effect your Intelligence Level) ");
+        Console.WriteLine("*ENTER NUMBER BETWEEN 1-6 TO INDICATE CHOICE*");
+        int sChoice = Convert.ToInt32(Console.ReadLine());
+        int[] strengthLvl = { 1, 2, 3, 4, 5, 6 };
+
+        while (sChoice > 6 || sChoice < 1)
+        {
+            Console.WriteLine("OPS THAT WASN'T AN OPTION... *ENTER NUMBER BETWEEN 1-6 TO INDICATE CHOICE*");
+            sChoice = Convert.ToInt32(Console.ReadLine());
+
+        }
+        int Y = sChoice - 1;
+
+        int i = 7 - sChoice;
+
+
+        Console.WriteLine("\n" + "Your Character is a: " +classType[X] + "\nWith a Strength Level of: " 
+            +strengthLvl[Y] + "\nAnd an Intelligence level of: " + i);
+        Console.WriteLine();
+
+
+
+        Console.ReadLine();
+    }
+    }
+
---
+++ testConsoleApp1/testConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+        //int[] numArray = new int[5];
+        //numArray[0] = 5; 
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 200;
+        //numArray[4] = 5000;
+
+
+        //int[] numArray1 = new int[] { 1, 2, 3, 4, 5 };
+
+
+        //int[] numArray2 = { 6, 20, 51, 200, 500 };
+
+        //numArray2[3] = 650;
+
+        //Console.WriteLine(numArray2[3]);
+        //Console.ReadLine();
+
+
+        List<int> intList = new List<int>();
+        intList.Add(1);
+        intList.Add(2);
+
+        Console.WriteLine(intList[0]);
+        Console.ReadLine();
+    }
+    }
+

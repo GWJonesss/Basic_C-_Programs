465c4947a8163a3d9acd1f814e723bc8609b5807
Gabriel Jones <gabrieljones900@yahoo.com>
2022-06-16 08:18:34 -0700

    MAIN METHOD SUBMISSION ASSIGNMENT
    C# AND UNITY COURSE Step: 218
    Create a class. In that class, create a method that will take in an integer, create a math operation for this integer (addition, subtraction, etc.), then return the answer as an integer.
    
    In the Main() method of the console app, instantiate the class and call the one method, passing in an integer. Display the result to the screen.
    
    Add a second method to the class with the same name that will take in a decimal, create a different math operation for it, then return the answer as an integer.
    
    In the Main() method of the console app, instantiate the class and call the second method, passing in a decimal. Display the result to the screen.
    
    Add a third method to the class, with the same name, that will take in a string, convert it to an integer if possible, do a different math operation to it, then return the answer as an integer.
    
    In the Main() method of the console app, instantiate the class and call the third method, passing in a string that equates to an integer. Display the result to the screen.

---
+++ MAIN_METHOD/MAIN_METHOD/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAIN_METHOD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Value = 5;
+
+            var operatorObject = new Math();
+            int result = operatorObject.Add(Value, 14);
+
+            Console.WriteLine("The result of 5 + 14 is " + result);
+
+            decimal Value2 = 5.5m;
+            int result2 = operatorObject.Add(Value2, 3);
+            Console.WriteLine("The result of 5.5 + 3 returned as an int is " + result2);
+
+            string Value3 = "24";
+            int result3 = operatorObject.Add(Value3, 2);
+
+            Console.WriteLine("The result of '24' + 2 returned as an int is " + result3);
+            Console.ReadLine();
+        }
+    }
+}

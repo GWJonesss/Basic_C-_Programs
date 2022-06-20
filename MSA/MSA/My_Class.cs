3a9e0d19b4d3d9d97b7d5f96cf99ba2a7444c9e0
Gabriel Jones <gabrieljones900@yahoo.com>
2022-06-20 15:08:13 -0700

    METHOD SUBMISSION ASSIGNMENT
    C# AND UNITY COURSE Step: 219
    
    Create a class. In that class, create a method that takes two integers as parameters. Make one of them optional. Have the method do a math operation and return an integer result.
    
    In the Main() method of the console app, instantiate the class.
    
    Ask the user to input two numbers, one at a time. Let them know they need not enter anything for the second number.
    
    Call the method in the class, passing in the one or two numbers entered.
    
    Try various combinations of numbers on the code, including having no second number.

---
+++ MSA/MSA/My_Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSA
+{
+    public class My_Class
+    {
+        public int Add(int data, params int[] value)
+        {
+
+            int i = 2 ;
+           
+            foreach(int num in value)
+            {
+                i *= num;
+            }
+
+            return data + i;
+        }
+    }
+}
---
+++ MSA/MSA/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var operatorObject = new My_Class();
+
+
+            Console.WriteLine("Please input a number");
+            int fstChoice = Convert.ToInt32(Console.ReadLine());
+
+           
+            Console.WriteLine("Optional input a second number");
+            try
+            {
+                int sndChoice = Convert.ToInt32(Console.ReadLine());
+
+                var result = operatorObject.Add(fstChoice, sndChoice);
+
+                Console.WriteLine("The result of the calculations is: " + result);
+
+                Console.ReadLine();
+
+            }
+            catch
+            {
+                
+
+                var result = operatorObject.Add(fstChoice);
+
+                Console.WriteLine("The result of the calculations is: " + result);
+
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}

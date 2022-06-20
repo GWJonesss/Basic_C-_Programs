20cb6bac5ed11eb862aa2e3e61aa0171ecc3e7c9
Gabriel Jones <gabrieljones900@yahoo.com>
2022-06-20 16:48:20 -0700

    METHOD CLASS SUBMISSION ASSIGNMENT
    C# AND UNITY COURSE
    Step: 220
    Create a class. In that class, create a void method that takes two integers as parameters. Have the method do a math operation on the first integer and display the second integer to the screen.
    
    In the Main() method of the console app, instantiate the class.
    
    Call the method in the class, passing in two numbers.
    
    Call the method in the class, specifying the parameters by name.

---
+++ Void_Method/Void_Method/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            V_Class b = new V_Class(); 
+            b.count(x: 10 ,y: 3); 
+           
+
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ Void_Method/Void_Method/V_Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Void_Method
+{
+    public class V_Class
+    { 
+
+         
+
+       public void count(int x, int y)
+            {
+                Console.WriteLine(x);
+                int z = 5 * y;
+            Console.WriteLine(z);
+            }
+       
+
+        
+    }
+    
+}

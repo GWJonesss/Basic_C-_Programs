5bae6b5156d40962831aed54b0692b498937c31e
Gabriel Jones <gabrieljones900@yahoo.com>
2022-06-20 19:08:10 -0700

    CLASS SUBMISSION ASSIGNMENT
    C# AND UNITY COURSE
    Step: 224
    Create a class. In that class, create a void method that outputs an integer. Have the method divide the data passed to it by 2.
    
    In the Main() method, instantiate that class.
    
    Have the user enter a number. Call the method on that number. Display the output to the screen. It should be the entered number, divided by two.
    
    Create a method with output parameters.
    
    Overload a method.
    
    Declare a class to be static.

---
+++ C_S_A/C_S_A/New_Class.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_S_A
+{
+    public class New_Class
+    {
+        public void div (int a)
+        {
+            int b = a / 2;
+            Console.WriteLine("Good job, the answer is " + b);
+
+            
+        }
+
+        
+        
+        
+
+    }
+}
